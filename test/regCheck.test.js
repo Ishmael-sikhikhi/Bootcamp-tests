a52dc935fcd872e9cb2e6507cb1c59a919328c28
Ishmael-sikhikhi <i.sikhikhi@gmail.com>
2021-04-07 15:34:48 +0200

    Commit

--- test/regCheck.test.js
+++ test/regCheck.test.js
@@ -1,8 +1,8 @@
 describe('regCheck' , function(){
-    it('should return true if Registration number is for GP, L, EC, MP ' , function(){
+    it('should return false if Registration number is not for MP ' , function(){
         assert.deepEqual(regCheck('DV 23 LP GP', 'MP'),false);
     });
-    it('should return true if Registration number is for GP, L, EC, MP ' , function(){
+    it('should return true if Registration number is for GP ' , function(){
         assert.equal(regCheck('DV 23 NB GP', 'GP'),true);
     });    
     it('should return false if Registration number is not for GP, L, EC, MP ' , function(){
